a976629229633e57e6787f02dca0c6cb5d08d310
Aditi_kharabe <67113507+aditirepo426@users.noreply.github.com>
2020-07-21 14:42:27 +0530

    VISUAL CODE STUDIO

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper {
+    constructor(x,y,radius){
+    var options = {
+        isStatic: false,
+        restitution: 0.3,
+        friction: 1,
+        density: 1.2
+    }
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.body = Bodies.circle(this.x,this.y,this.radius/2,options);
+    this.image = loadImage("paperwh.jpg");
+    World.add(world,this.body);
+    
+} 
+display(){
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    //ellipse(0,0,this.radius,this.radius);
+    imageMode(CENTER);
+    image(this.image,50,100,100,100);
+    pop();
+
+}
+};
+
+
